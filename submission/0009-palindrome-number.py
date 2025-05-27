fa562625bbc80df0926b03172153677f55e3cda7
chasehmathis <chase1mathis@gmail.com>
2025-05-27 13:26:01 +0200

    Deleting a fun albeit small leetcode quiz generator

--- submission/0009-palindrome-number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-
-        s = str(x)
-        for i in range(len(s)):
-            if s[i] != s[len(s)-1-i]:
-                return False
-        return True
